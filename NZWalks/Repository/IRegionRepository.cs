16d9d2d3b221a6c3d1704bf90b019e99209aab0c
Ibne Nahian <evilprince2009@yahoo.com>
2024-11-06 16:22:08 +0600

    #

--- NZWalks/Repository/IRegionRepository.cs
+++ NZWalks/Repository/IRegionRepository.cs
@@ -8,15 +8,7 @@ namespace NZWalks.Repository
         Task<List<Region>> GetAllAsync();
         Task<Region?> GetByIdAsync(Guid id);
         Task<Region?> DeleteAsync(Guid id);
-
-        // public IActionResult Update(Guid id, UpdateRegionDTO new_region)
-
         Task<RegionDTO> UpdateAsync(Guid id, UpdateRegionDTO new_region);
-
-        // public IActionResult Create([FromBody] AddRegionDTO incoming_region)
-
         Task<Region> CreateAsync(AddRegionDTO incoming_region);
-
-      
     }
 }
