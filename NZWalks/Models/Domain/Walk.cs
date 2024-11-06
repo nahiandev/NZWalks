eeeeed7249cb393865fc953bb42ea8090344afda
Ibne Nahian <evilprince2009@yahoo.com>
2024-11-06 16:25:17 +0600

    #

--- NZWalks/Models/Domain/Walk.cs
+++ NZWalks/Models/Domain/Walk.cs
@@ -3,13 +3,13 @@
     public class Walk
     {
         public Guid Id { get; set; }
-        public string Name { get; set; }
-        public string Description { get; set; }
+        public required string Name { get; set; }
+        public required string Description { get; set; }
         public double LengthInKM { get; set; }
         public string? WalkImamgeUrl { get; set; }
         public Guid DifficultyId { get; set; }
         public Guid RegionId { get; set; }
-        public Difficulty Difficulty { get; set; }
+        public required Difficulty Difficulty { get; set; }
         
     }
 }
