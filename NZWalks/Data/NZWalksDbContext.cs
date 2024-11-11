c5af9b17ab7e26ee322ba69a97290b9b064ed4fb
Ibne Nahian <evilprince2009@yahoo.com>
2024-11-12 00:50:05 +0600

    # making DbContext domain safe

--- NZWalks/Data/NZWalksDbContext.cs
+++ NZWalks/Data/NZWalksDbContext.cs
@@ -5,7 +5,7 @@ namespace NZWalks.Data
 {
     public class NZWalksDbContext : DbContext
     {
-        public NZWalksDbContext(DbContextOptions options) : base(options) { }
+        public NZWalksDbContext(DbContextOptions<NZWalksDbContext> options) : base(options) { }
 
         public DbSet<Region> Regions { get; set; }
         public DbSet<Difficulty> Difficulties { get; set; }
