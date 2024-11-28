f2db46fe87b6591c4d53602b423544b0dce36f25
Ibne Nahian <evilprince2009@yahoo.com>
2024-11-28 15:44:33 +0600

    unified model validation

--- src/Models/Domain/Region.cs
+++ src/Models/Domain/Region.cs
@@ -1,8 +1,11 @@
-﻿namespace NZWalks.Models.Domain
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NZWalks.Models.Domain
 {
     public class Region
     {
         public Guid Id { get; set; }
+        
         public string Code { get; set; }
         public string Name { get; set; }
         public string? RegionImageUrl { get; set; }
