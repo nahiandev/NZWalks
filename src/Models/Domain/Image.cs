534199eaba791ca7caa853ac6a30d2e803365233
Ibne Nahian <evilprince2009@yahoo.com>
2024-11-30 14:48:16 +0600

    image domain

---
+++ src/Models/Domain/Image.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace NZWalks.Models.Domain
+{
+    public class Image
+    {
+        public Guid Id { get; set; }
+
+        [NotMapped]
+        public IFormFile File { get; set; }
+        public string? Description { get; set; }
+        public string Extension { get; set; }
+        public long SizeInBytes { get; set; }
+        public string PathToFile { get; set; }
+    }
+}
